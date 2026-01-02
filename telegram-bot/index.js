dbc7923b249d3478a2b5b5fc1f0ecdcafab54c49
Souvik364 <geetawisdom1@gmail.com>
2026-01-02 23:19:16 +0530

    Create index.js

---
+++ telegram-bot/index.js
@@ -0,0 +1,19 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Load features (MODULES)
+require('./features/welcome')(bot);
+require('./features/forceJoin')(bot);
+require('./features/menu')(bot);
+require('./features/autoReply')(bot);
+require('./features/shop')(bot);
+require('./features/support')(bot);
+
+bot.launch();
+
+console.log("🤖 Bot is running...");
+
+// Enable graceful stop (important for Render)
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
